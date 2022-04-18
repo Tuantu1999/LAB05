6bf5628b3ea79a7e42eae612373cc9171dada109
Tuan Tu <tuantuhoang1999@gmail.com>
2022-04-18 11:50:20 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,35 +1,47 @@
 import React, { Component } from "react";
-import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
-// import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Contact from "./ContactComponent";
+import Home from "./HomeComponent";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { DISHES } from "../shared/dishes";
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 
 class Main extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
   render() {
-
-    const HomePage = () => {
-      return(
-          <Home 
-          />
+    const HomePage = (props) => {
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
-    }
+    };
     return (
       <div>
         <Header />
         <Switch>
-          <Route path = "/home" component = { HomePage } />
-          <Route exact path = "/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
